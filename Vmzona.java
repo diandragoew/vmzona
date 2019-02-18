dc40aacc9d7c288baeca158fbe0514e98d4a199b
Djemile <djemile14@abv.bg>
2019-02-18 02:11:57 +0200

    ...

--- Vmzona.java
+++ Vmzona.java
@@ -15,6 +15,18 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
 
+/*
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+*/
+
 public class Vmzona {
 
     public static final int MIN_VOTE = 1;
